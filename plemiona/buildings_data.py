8434bb69cc24f55d545718821b95922522855479
Flicksan10 <Flicksan10@gmail.com>
2023-11-14 23:28:53 +0100

    added file with data for function with will be triggered every 10 seconds

---
+++ plemiona/buildings_data.py
@@ -0,0 +1,41 @@
+sawmill_data = [
+    {"lvl": 1, "wood": 50, "clay": 60, "iron": 40, "people_needed": 5, "total_population": 5, "performance": 15},
+    {"lvl": 2, "wood": 63, "clay": 77, "iron": 50, "people_needed": 1, "total_population": 6, "performance": 17},
+    {"lvl": 3, "wood": 78, "clay": 98, "iron": 63, "people_needed": 1, "total_population": 7, "performance": 20},
+    {"lvl": 4, "wood": 98, "clay": 124, "iron": 77, "people_needed": 1, "total_population": 8, "performance": 23},
+    {"lvl": 5, "wood": 122, "clay": 159, "iron": 96, "people_needed": 1, "total_population": 9, "performance": 27},
+    {"lvl": 6, "wood": 153, "clay": 202, "iron": 120, "people_needed": 1, "total_population": 10, "performance": 32},
+    {"lvl": 7, "wood": 191, "clay": 258, "iron": 149, "people_needed": 2, "total_population": 12, "performance": 37},
+    {"lvl": 8, "wood": 238, "clay": 329, "iron": 185, "people_needed": 2, "total_population": 14, "performance": 43},
+    {"lvl": 9, "wood": 298, "clay": 419, "iron": 231, "people_needed": 2, "total_population": 16, "performance": 50},
+    {"lvl": 10, "wood": 373, "clay": 534, "iron": 287, "people_needed": 2, "total_population": 18, "performance": 58}
+]
+
+
+
+clay_pit_data = [
+    {"lvl": 1, "wood": 65, "clay": 50, "iron": 40, "people_needed": 10, "total_population": 10, "performance": 15},
+    {"lvl": 2, "wood": 83, "clay": 63, "iron": 50, "people_needed": 1, "total_population": 11, "performance": 17},
+    {"lvl": 3, "wood": 105, "clay": 80, "iron": 62, "people_needed": 2, "total_population": 13, "performance": 20},
+    {"lvl": 4, "wood": 133, "clay": 101, "iron": 76, "people_needed": 2, "total_population": 15, "performance": 23},
+    {"lvl": 5, "wood": 169, "clay": 128, "iron": 95, "people_needed": 2, "total_population": 17, "performance": 27},
+    {"lvl": 6, "wood": 215, "clay": 162, "iron": 117, "people_needed": 2, "total_population": 19, "performance": 32},
+    {"lvl": 7, "wood": 273, "clay": 205, "iron": 145, "people_needed": 3, "total_population": 22, "performance": 37},
+    {"lvl": 8, "wood": 346, "clay": 259, "iron": 180, "people_needed": 3, "total_population": 25, "performance": 43},
+    {"lvl": 9, "wood": 440, "clay": 328, "iron": 224, "people_needed": 4, "total_population": 29, "performance": 50},
+    {"lvl": 10, "wood": 559, "clay": 415, "iron": 277, "people_needed": 4, "total_population": 33, "performance": 58}
+]
+
+
+iron_mine_data = [
+    {"lvl": 1, "wood": 75, "clay": 65, "iron": 70, "people_needed": 10, "total_population": 10, "performance": 15},
+    {"lvl": 2, "wood": 94, "clay": 83, "iron": 87, "people_needed": 2, "total_population": 12, "performance": 17},
+    {"lvl": 3, "wood": 118, "clay": 106, "iron": 108, "people_needed": 2, "total_population": 14, "performance": 20},
+    {"lvl": 4, "wood": 147, "clay": 135, "iron": 133, "people_needed": 2, "total_population": 16, "performance": 23},
+    {"lvl": 5, "wood": 184, "clay": 172, "iron": 165, "people_needed": 3, "total_population": 19, "performance": 27},
+    {"lvl": 6, "wood": 231, "clay": 219, "iron": 205, "people_needed": 3, "total_population": 22, "performance": 32},
+    {"lvl": 7, "wood": 289, "clay": 279, "iron": 254, "people_needed": 4, "total_population": 26, "performance": 37},
+    {"lvl": 8, "wood": 362, "clay": 352, "iron": 316, "people_needed": 4, "total_population": 30, "performance": 43},
+    {"lvl": 9, "wood": 453, "clay": 454, "iron": 391, "people_needed": 5, "total_population": 35, "performance": 50},
+    {"lvl": 10, "wood": 567, "clay": 579, "iron": 485, "people_needed": 6, "total_population": 41, "performance": 58}
+]
